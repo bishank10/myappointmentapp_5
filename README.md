13325e862bd3fb885c424133d44f4ba53b1493eb
bishank <chandbishank@gmail.com>
2020-10-04 22:43:45 -0600

    updated the date and time code

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # myappointmentapp_5
+PsuedoCode
+Get current time and date using day.js create a body element inside and append all the necessary attributes. use of loops to create all the necessary elements.
+
+
+Improvements
+Better styling change to allow planes for the whole day display weather location and quote of the day create a save all button that saves all the text boxes at once create a clear button that clears the textboxes.
+
+Build a Calender and use this as a template for the day planner for each day.
+
+Developer
+Bishank Bohara date: 10/04/2020
--- appointment.js
+++ appointment.js
@@ -1,24 +1,11 @@
 
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours (function)
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-    // use the logic here to get the  color changed
-// WHEto N I click into a timeblock
-    // THEN I can enter an event
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// use of set and get for the the local storage value
-// WHEN I refresh the page
-// THEN the saved events persist   get the set value from the local storage
 
-// i need 
 
 $(document).ready(function(){
     // console.log("hello");
 
 
-    var scheduleContent;
+    var hours =[9, 10,11,12,1,2,3,]
     var businessHours = [ '9 AM',"10 AM", "11 AM","12 PM","1 PM","2 PM","3 PM"];
     var indx = 0;
     var appointmentDetailText = "";
@@ -27,21 +14,10 @@ $(document).ready(function(){
 
   
     // FUNCTION TO DISPLAY THE CURRENT DATE WHEN THE APP IS OPENED
-    // function showWeather(cityName){
-    //  displayTime(){
-
-         //  ISO format
-    // '2018-04-04T16:00:00.000Z'
-    // If no date is supplied to days(), it assumes NOW
-    var defFormat = dayjs().format();
-    // You can make a variable for any date object you need
-    var garyBirthday = dayjs("1965-12-07T16:00:00.000Z")
-    var anyOldDay = dayjs("1995-06-07T16:00:00.000Z");
-    // You can format any date
-    var format1 = garyBirthday.format("YYYY, MM, DD");
-    // Determine if one date is after another
-    var isAfter = garyBirthday.isAfter(anyOldDay, 'hour');
-    console.log(isAfter);
+    
+    var now = dayjs().format("YYYY, MM, DD");
+    
+    $(".lead").append(now);
         
   
 
@@ -57,55 +33,41 @@ $(document).ready(function(){
 
        var  appointmentDetailEl = $("<input>");
         appointmentDetailEl.attr("placeholder","text..");
+        var txtValue = appointmentDetailEl.val();
+        appointmentDetailEl.attr("value",txtValue);
         numberContainer.append(appointmentDetailEl);
-         txtValue = appointmentDetailEl.text();
         var saveBtn = $("<button>");
         saveBtn.text("SAVE");
         numberContainer.append(saveBtn);
+
         
         }
+
+        localStorage.setItem("timeDescription", JSON.stringify(txtValue));
+        
 
         // saving to the local storage
-        function storeData(){
-            
-            saveBtn.on("click",function(txtValue){
-            if (txtValue !== null){
-            localStorage.setItem("timeDescription", txtValue);
-            }  else{
-                alert("no input");
-            } 
-              
+        // for (var i = 0; i<businessHours.length;i++) {
+            saveBtn.on("click", function(){
+                if(txtValue !== null){
+                    localStorage.setItem("plan", JSON.stringify(txtValue));
+                }else{
+                    alert("please enter something")
+                }
             })
-        }
+    
+            
+        
 
-        // function that shows the plan for the time even after the refresh
+        // function that shows the description for that time
         function displayOnScreen(){
             var description = parseInt(localStorage.getItem("timeDescription"));
-            appointmentDetailEl.val(description);
+            appointmentDetailEl.append(description);
             
 
         }
         
 
-
-        // create a function to save the data in local storage so that it stays there even after refresh
-
-        // saveBtn.on("click",function(){
-        //    console.log("hello world");
-        // })
-
-       
-
-        
-    
-    
-
-
-    // create a function to change colour of the actual hour and past and the future
-
-
-
-    // create a function to save the data in local storage so that it stays there even after refresh
     
 
 
